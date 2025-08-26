efdc9f717d162a94554deaffc13c2d3f5cc8f5d8
llper <llper@llper.com>
2025-08-26 14:52:58 +0200

    Add timetracker-frontend/src/environments/environment.ts 1756212777976

---
+++ timetracker-frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  keycloak: {
+    url: 'http://idp.localhost',
+    realm: 'tt-realm',
+    clientId: 'PEAX-agent'
+  }
+};
